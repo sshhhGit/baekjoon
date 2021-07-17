657f4debb7b940a40d6f4210a5cf2eb8171410d1
sshhhGit <seungho0497@hanmail.net>
2021-07-17 22:58:30 +0900

    Add new algorithm

---
+++ src/Main2742.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i;
+        for(i=n;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
