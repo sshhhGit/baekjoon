94d2885f39266ecf824742e680f400a7dffabe0a
sshhhGit <seungho0497@hanmail.net>
2021-07-24 23:04:55 +0900

    Add new algorithm

---
+++ src/Main8393.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i, sum=0;
+        for(i=1;i<=n;i++){
+            sum+= i;
+        }System.out.println(sum);
+    }
+}
