bbb3ec13e68e304e6937a9be538d7205d3ef7865
sshhhGit <seungho0497@hanmail.net>
2021-10-03 23:40:43 +0900

    Add new algorithm

--- src/Main11720.java
+++ src/Main11720.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main11720 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String str = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
         int sum = 0;
         for (int i=0;i<N;i++){
             sum += Character.getNumericValue(str.charAt(i));
