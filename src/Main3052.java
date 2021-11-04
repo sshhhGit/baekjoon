61bd970a7b8cab35641fdac1ee3390eb10331321
sshhhGit <seungho0497@hanmail.net>
2021-11-04 21:02:04 +0900

    Add new algorithm

--- src/Main3052.java
+++ src/Main3052.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main3052 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a, b, cnt = 0;
-        String[] str = new String[10];
+        int a, cnt = 0;
+        boolean[] str = new boolean[42];
         for (int i = 0; i < 10; i++){
             a = Integer.parseInt(br.readLine());
-            b = a%42;
-            str[i] = String.valueOf(b);
-            if (str[i].equals(b) ){
-                System.out.println("ㅇㅇ");
+            str[a%42] = true;
+        }
+        for (int i = 0; i < 42; i++){
+            if (str[i]){
                 cnt++;
             }
         }
         System.out.println(cnt);
-        System.out.println(Arrays.toString(str));
     }
 }
