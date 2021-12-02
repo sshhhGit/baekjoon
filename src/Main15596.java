0cf1e2c6b49e037812e61013e6165f0dbfa1c012
Seungho <83917282+sshhhGit@users.noreply.github.com>
2021-12-02 17:28:43 +0900

    Update Main15596.java

--- src/Main15596.java
+++ src/Main15596.java
@@ -1,8 +1,16 @@
 /**
-   Java: long sum(int[] a); (클래스 이름: Test)
+ * Java: long sum(int[] a); (클래스 이름: Test)
         a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
-        리턴값: a에 포함되어 있는 정수 n개의 합
- */
+        리턴값: a에 포함되어 있는 정수 n개의 합*/
 
 public class Main15596 {
+    class Test{
+        long sum(int[] a) {
+            long ans = 0;
+            for (int i = 0; i<a.length; i++){
+                ans += a[i];
+            }
+            return ans;
+        }
+    }
 }
